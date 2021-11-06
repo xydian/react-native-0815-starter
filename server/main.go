9d1076649ce7824d21b858fa1f90ac7982c560c2
Chris <chrisjb@mailbox.org>
2021-11-06 22:27:18 +0100

    Implementing envconfig to read config from environment variables

--- server/main.go
+++ server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "log"
+import (
+	"github.com/kelseyhightower/envconfig"
+	"log"
+)
+
+type Config struct {
+	Debug bool `required:"true"`
+
+	DbPort string `required:"true"`
+	DbHost string `required:"true"`
+	DbUsername string `required:"true"`
+	DbPassword string `required:"true"`
+}
 
 func main(){
-	log.Println("hello")
+	var config Config
+	err := envconfig.Process("Server", &config)
+	if err != nil {
+		log.Fatalf("Error reading config from environment variables %s", err.Error())
+	}
+
+	log.Println(config.DbPort)
 }
