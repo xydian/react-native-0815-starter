b3ea816b5a3acf6bb71209e9dd95b44d2b69529c
Chris <chrisjb@mailbox.org>
2021-11-06 21:04:30 +0100

    Add button to toggle theme

--- app/screens/Details.tsx
+++ app/screens/Details.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Divider, Icon, IconProps, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
+import { Button, Divider, Icon, IconProps, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
 import { NavigationProp } from '@react-navigation/core'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useThemeStore } from '../stores/theme'
 
 const BackIcon = (props: IconProps) => (
 	<Icon {...props} name='arrow-back' />
@@ -17,12 +18,15 @@ export function DetailsScreen(props: { navigation: NavigationProp<Record<string,
 		<TopNavigationAction icon={BackIcon} onPress={navigateBack}/>
 	)
 
+	const toggleTheme = useThemeStore(state => state.toggleTheme)
+
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<TopNavigation title='MyApp' alignment='center' accessoryLeft={BackAction}/>
 			<Divider/>
 			<Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 				<Text category='h1'>DETAILS</Text>
+				<Button onPress={toggleTheme}>toggle theme</Button>
 			</Layout>
 		</SafeAreaView>
 	)
