b8665385260c643a7636ed96e4e7afe00e16c7c7
Chris <chrisjb@mailbox.org>
2021-11-06 20:54:05 +0100

    Implement basic navigatioN

--- app/App.tsx
+++ app/App.tsx
@@ -1,30 +1,21 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
 
 import * as eva from '@eva-design/eva'
-import { ApplicationProvider, Text, IconRegistry } from '@ui-kitten/components'
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
+import { AppNavigator } from './RootNavigation'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
 	return (
 		<>
 			<IconRegistry icons={EvaIconsPack} />
 			<ApplicationProvider {...eva} theme={eva.light}>
-				<View style={styles.container}>
-					<Text>Open up App.tsx to start working on your app!</Text>
-					<StatusBar style="auto" />
-				</View>
+				<SafeAreaProvider>
+					<AppNavigator/>
+				</SafeAreaProvider>
 			</ApplicationProvider>
+
 		</>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
